ff0b657ac43c2f1162b1968b76b0d76740fda48a
Rijin Mk <rijin.mk9@gmail.com>
2021-07-19 01:19:32 +0400

    Initialize react matterjs

--- matter-js-1/src/App.tsx
+++ matter-js-1/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import MatterContainer from './MatterContainer/MatterContainer';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <MatterContainer />
     </div>
   );
 }
